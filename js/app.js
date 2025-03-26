cb6a18ce760e2df9f98ac22aaacf40e255078bae
yusuf almahroos <yousif4shukri@gmail.com>
2025-03-26 03:30:15 +0300

    game page css

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 const mainStartBtnEl = document.getElementById('main-start-btn');
 const mainPageEl     = document.getElementById('main-page')
+const gamePageEl     = document.getElementById('game-page');
 
 mainStartBtnEl.addEventListener('click', () => 
 {
   mainPageEl.style.display = 'none';
+  gamePageEl.style.display = 'flex'
 })
