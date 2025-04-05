9f16439ef70af23c0d8ea060858d843af0fa75b9
yusuf almahroos <yousif4shukri@gmail.com>
2025-04-05 23:50:00 +0300

    js functions

--- js/app.js
+++ js/app.js
@@ -7,3 +7,46 @@ mainStartBtnEl.addEventListener('click', () =>
   mainPageEl.style.display = 'none';
   gamePageEl.style.display = 'flex'
 })
+
+const foodTypes = 
+[
+  {name: "Sambosa" , moves: ["UP", "ADD", "LEFT", "RIGHT"]},
+  {name: "Springrolls" , moves: ["UP", "LEFT", "RIGHT", "ADD", "UP", "UP "]}
+];
+
+let gameDiff = "easy";
+
+function game_changeShape ()
+{
+
+}
+
+function game_progressBar()
+{
+
+}
+
+function game_updateQueue ()
+{
+
+}
+
+function game_timer ()
+{
+
+}
+
+function game_process ()
+{
+
+}
+
+function  game_render()
+{
+
+}
+
+function game_init()
+{
+
+}
