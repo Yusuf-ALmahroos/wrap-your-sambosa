bcf66e0d6015539789e0ce0a2f079899695edf50
yusuf almahroos <yousif4shukri@gmail.com>
2025-04-07 01:38:35 +0300

    initial js code and btns css

--- js/app.js
+++ js/app.js
@@ -1,20 +1,85 @@
-const mainStartBtnEl = document.getElementById('main-start-btn');
-const mainPageEl     = document.getElementById('main-page')
-const gamePageEl     = document.getElementById('game-page');
-
-mainStartBtnEl.addEventListener('click', () => 
-{
-  mainPageEl.style.display = 'none';
-  gamePageEl.style.display = 'flex'
-})
-
 const foodTypes = 
 [
   {name: "Sambosa" , moves: ["UP", "ADD", "LEFT", "RIGHT"]},
   {name: "Springrolls" , moves: ["UP", "LEFT", "RIGHT", "ADD", "UP", "UP "]}
 ];
 
+const foodQueue = [];
+
 let gameDiff = "easy";
+
+const mainStartBtnEl   = document.getElementById('main-start-btn');
+const mainPageEl       = document.getElementById('main-page')
+const gamePageEl       = document.getElementById('game-page');
+const difficultyBtnEls = document.querySelectorAll('.difficulty-btn');
+
+
+function game_start()
+{
+  switch(gameDiff)
+  {
+    case "easy":
+      foodQueue.length = 0;
+      foodQueue.push(foodTypes[0], foodTypes[1], foodTypes[0]);
+      break;
+    
+    case "mid":
+      foodQueue.length = 0;
+      foodQueue.push(foodTypes[1], foodTypes[1], foodTypes[0], foodTypes[0]);
+      break;
+    
+    case "hard":
+      foodQueue.length = 0;
+      foodQueue.push(foodTypes[0], foodTypes[1], foodTypes[1], foodTypes[0],  foodTypes[1],);
+      break;
+  }
+  mainPageEl.style.display = 'none';
+  gamePageEl.style.display = 'flex';
+}
+
+function game_setMainBtnsListeners()
+{
+  mainStartBtnEl.addEventListener('click', () => 
+  {
+    game_start();
+  })
+      
+  difficultyBtnEls.forEach(btn => 
+  {
+    btn.addEventListener('click', () => 
+    {
+      difficultyBtnEls.forEach(button => button.classList.remove('active'))
+      btn.classList.add('active');
+      gameDiff = btn.dataset.level;
+    })
+  })
+}
+
+const resetBtnEL   = document.getElementById("reset-btn");
+const keypadBtnEls = document.querySelectorAll('.keypad-btn');
+
+function game_resetRound ()
+{
+
+}
+
+function game_processClicked (clicked)
+{
+  
+}
+
+function game_setGameBtnsListeners()
+{
+  resetBtnEL.addEventListener('click', () => game_resetRound())
+
+  keypadBtnEls.forEach(btn => 
+  {
+    btn.addEventListener('click', () => 
+    {
+      game_processClicked(btn.dataset.type)
+    })
+  })
+}
 
 function game_changeShape ()
 {
@@ -23,8 +88,8 @@ function game_changeShape ()
 
 function game_progressBar()
 {
-
 }
+
 
 function game_updateQueue ()
 {
@@ -36,11 +101,6 @@ function game_timer ()
 
 }
 
-function game_process ()
-{
-
-}
-
 function  game_render()
 {
 
@@ -48,5 +108,8 @@ function  game_render()
 
 function game_init()
 {
-
+  game_setMainBtnsListeners();
+  game_setGameBtnsListeners();
 }
+
+game_init();
